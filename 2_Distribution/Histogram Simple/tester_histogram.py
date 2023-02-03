5d09a729e32770e84bb2158185442480a939ec0f
EKChikui <EKChikui@gmail.com>
2023-02-02 23:29:33 -0300

    Update tester_histogram.py

--- 2_Distribution/Histogram Simple/tester_histogram.py
+++ 2_Distribution/Histogram Simple/tester_histogram.py
@@ -7,3 +7,9 @@ from plot_histogram_v01 import *
 
 filename = "hmeq.csv"
 df = pd.read_csv(filename, sep=",", encoding="utf-8")
+
+cols_hist = ["LOAN", "MORTDUE", "VALUE", "YOJ", "DEROG",
+             "DELINQ", "CLAGE", "NINQ", "CLNO", "DEBTINC"]
+
+for col in cols_hist:
+    plot_histogram(df[col])
