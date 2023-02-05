b8a516385ba22f09e4f258ff1cd7dfc88f3647bd
EKChikui <EKChikui@gmail.com>
2023-02-05 09:22:55 -0300

    added two more tests for Histogram plot

--- 2_Distribution/Histogram Simple/tester_histogram.py
+++ 2_Distribution/Histogram Simple/tester_histogram.py
@@ -15,3 +15,4 @@ cols_hist = ["LOAN", "MORTDUE", "VALUE", "YOJ", "DEROG",
 plot_histogram(df["LOAN"], bins="sqrt")
 plot_histogram(df["MORTDUE"], bins="sqrt", kde=False)
 plot_histogram(df["CLAGE"], bins="freedman", grid_axis="both")
+plot_histogram(df["MORTDUE"], title="Histogram - HMEQ MortDue", bins="sqrt", savefig=True)
