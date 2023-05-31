abae6cad18a2e19da6fa2e598dbcde4943e6a402
Eduardo Chikui <EKChikui@gmail.com>
2023-05-31 19:50:25 -0300

    Create tester_boxplot.py

---
+++ 1_Comparison/BoxPlot/tester_boxplot.py
@@ -0,0 +1,18 @@
+
+# Libraries
+import numpy as np
+import pandas as pd
+
+from plot_boxplot_v01 import *
+
+# Setup/Config
+
+
+# Program --------------------------------------------------------------
+filename = "iris.csv"
+
+df = pd.read_csv(filename, sep=",", encoding="utf-8")
+cols = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+
+plot_boxplot(df, columns=cols, title="Iris - BoxPlot")
+
