b238128a8a25aa60a4447283902b9dda5e18d65a
Eduardo Chikui <EKChikui@gmail.com>
2023-06-10 16:48:46 -0300

    Update tester_boxplot.py

--- 1_Comparison/BoxPlot/tester_boxplot.py
+++ 1_Comparison/BoxPlot/tester_boxplot.py
@@ -14,5 +14,5 @@ filename = "iris.csv"
 df = pd.read_csv(filename, sep=",", encoding="utf-8")
 cols = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
 
-plot_boxplot(df, columns=cols, title="Iris - BoxPlot")
+plot_boxplot(df, columns=cols, title="Iris - BoxPlot", savefig=True)
 
