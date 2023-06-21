047ba1210359fcadcf4ee1c134625240706345f1
Eduardo Chikui <EKChikui@gmail.com>
2023-06-20 21:59:47 -0300

    Update plot_blandaltman.py

--- 1_Comparison/Bland_Altman/plot_blandaltman.py
+++ 1_Comparison/Bland_Altman/plot_blandaltman.py
@@ -1,34 +1,35 @@
-# Bland Altman Analysis
+# Bland Altman Analysis ------------------------------------------------
 
-def plot_blandaltman(ground_truth, comparison, title=None, label1="ground_truth",
-                     label2="comparison", savefig=False, verbose=True):
+# Versions 
+# 01 - Mar 22nd, 2023 - Starter
+# 02 - Jun 20th, 2023 - Applying adjusts to style
+# 03 - 
+
+
+# Insights
+# Add variable to control plot size for histogram,
+
+
+
+# Libraries
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+
+
+def plot_blandaltman(y_true, y_pred, title=None, bins="sqrt", savefig=False, verbose=True):
     """
     Performs Bland-Altman analysis to evaluate a bias between the mean
     differences, and to estimate an agreement interval, within which
     95% of the differences.
 
     """
-
-    # Libraries
-    import numpy as np
-    import pandas as pd
-
-    import matplotlib.pyplot as plt
-    import matplotlib.gridspec as gridspec
-    
-
-    # Versions ---------------------------------------------------------
-    # 01 - Mar 22nd, 2023
-    # 02 -
-
-
-    # Insights
-
-
-
-    # Program ----------------------------------------------------------
-    ground_truth = np.array(ground_truth)
-    comparison = np.array(comparison)
+    # Data Preparation
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
 
 
     # Data
@@ -37,8 +38,8 @@ def plot_blandaltman(ground_truth, comparison, title=None, label1="ground_truth"
 
 
     # Statistic
-    mean = np.mean([comparison, ground_truth], axis=0)
-    diff = comparison - ground_truth
+    mean = np.mean([y_pred, y_true], axis=0)
+    diff = y_pred - y_true
 
     md = np.mean(diff)
     sd = np.std(diff)
@@ -51,15 +52,28 @@ def plot_blandaltman(ground_truth, comparison, title=None, label1="ground_truth"
     y_dist = max(np.abs(np.min(diff)), np.abs(np.max(diff)))
     y_lower = (-1) * (y_dist * 1.1)
     y_upper = (y_dist * 1.1)
-    # Turn "10" and 1.1 into a variable to be adjusted by **kwargs
+    
 
     # Bins
-    bins = int(np.sqrt(len(mean)) + 0.5)
+    # more info: https://numpy.org/doc/stable/reference/generated/numpy.histogram_bin_edges.html
+    bins_list = ["fd", "doane", "scott", "stone", "rice", "sturges", "sqrt"]
 
+    if(isinstance(bins, int) == True):
+        no_bins = bins
+
+    elif(bins_list.count(bins) == 1):
+        no_bins = np.histogram_bin_edges(diff, bins=bins).size
+
+    else:
+        print(f' >>> Error: "bins" option not valid. Using "sqrt" as forced option')
+        no_bins = np.histogram_bin_edges(diff, bins="sqrt").size
+    
 
     # Plot
+    plt.rcParams["font.family"] = "Helvetica"
+                 
     fig = plt.figure(figsize=[8, 4.5])
-    grd = fig.add_gridspec(nrows=1, ncols=2, width_ratios=[8, 2])
+    grd = fig.add_gridspec(nrows=1, ncols=2, width_ratios=[7.5, 2.5])
 
     ax0 = fig.add_subplot(grd[0, 0])
     ax1 = fig.add_subplot(grd[0, 1], sharey=ax0)
@@ -70,7 +84,7 @@ def plot_blandaltman(ground_truth, comparison, title=None, label1="ground_truth"
     ax0.scatter(mean, diff, s=30, color="navy", edgecolor="white", alpha=0.7, zorder=20)
 
     ax0.axhline(y=0, color="black", linestyle="-", linewidth=0.8, zorder=19)
-    ax0.axhline(y=md, color="red", linestyle="-", linewidth=0.8, zorder=18)
+    ax0.axhline(y=md, color="red", linestyle="-", linewidth=0.8, label = "Bias", zorder=18)
     ax0.axhline(y=(md - 1.96*sd), color="grey", linestyle="--", linewidth=0.8, zorder=17)
     ax0.axhline(y=(md + 1.96*sd), color="grey", linestyle="--", linewidth=0.8, zorder=16)
     ax0.fill_between(x=[x_lower-x_step, x_upper+x_step], y1=(md-1.96*sd), y2=(md+1.96*sd), color="lightgrey")
@@ -78,7 +92,7 @@ def plot_blandaltman(ground_truth, comparison, title=None, label1="ground_truth"
     ax0.set_xlabel("mean", loc="center")
     ax0.set_ylabel("difference", loc="center")
     ax0.grid(axis="both", color="grey", linestyle="--", linewidth=0.5)
-    ax0.set_xlim(left=(x_lower-x_step), right=(x_upper+x_step))
+    ax0.set_xlim(left=(x_lower - x_step), right=(x_upper + x_step))
     ax0.set_ylim(bottom=y_lower, top=y_upper)
 
     #ax1 = Histogram of difference
