3252a978b5dc2f6bd77dcd1797e50188f26f6b2a
Eduardo Chikui <EKChikui@gmail.com>
2023-06-20 21:59:52 -0300

    Update plot_heatmap.py

--- 4_Relationship/Heatmap/plot_heatmap.py
+++ 4_Relationship/Heatmap/plot_heatmap.py
@@ -1,13 +1,37 @@
 
+# Heatmap --------------------------------------------------------------
+
+# Versions 
+# 01 - Jan 31st, 2021 - Starter
+# 02 - Jan 31st, 2021 - Removing Duplicated Info
+# 03 - Feb 03rd, 2021 - Adjusting size for printing (A4 Ratio)
+# 04 - Feb 16th, 2021 - Adding **kwargs
+# 05 - Apr 21th, 2021 - Simplifying Data entry for just Data, removing
+#                       Columns (Labels) info
+# 06 - Sep 01st, 2021 - Adjusting (small corrections)
+# 07 - Apr 21st, 2022 - Adding Spearman and adjusting kwargs
+#                       Added correlation method to the title/filename
+# 08 - Jan 26th, 2023 - Adjusting new strategies
+# 09 - Jul 20th, 2023 - New standards and setup
+# 10 - 
+
+# Insights
+# Add rotation to x_axis labels (need to check anchor),
+#
+
+
+# Libraries
 import numpy as np
 import pandas as pd
+
+from pandas.api.types import is_numeric_dtype
 
 import matplotlib.pyplot as plt
 
 
 def plot_heatmap(DataFrame, title=None, columns="all", decimals=2,
-                 method="pearson", colormap="Blues", fontsize=9,
-                 savefig=False, verbose=True):
+                 method="pearson", colormap="Blues", savefig=False,
+                 verbose=True):
     """
     Plots a triangular **Heatmap** for **correlations** between variables,
     using Pearson, Spearman or Kendall methods.
@@ -25,23 +49,6 @@ def plot_heatmap(DataFrame, title=None, columns="all", decimals=2,
 
     """
 
-    # Versions ----------------------------------------------------------
-    # 01 - Jan 31st, 2021 - Starter
-    # 02 - Jan 31st, 2021 - Removing Duplicated Info
-    # 03 - Feb 03rd, 2021 - Adjusting size for printing (A4 Ratio)
-    # 04 - Feb 16th, 2021 - Adding **kwargs
-    # 05 - Apr 21th, 2021 - Simplifying Data entry for just Data, removing
-    #                       Columns (Labels) info
-    # 06 - Sep 01st, 2021 - Adjusting (small corrections)
-    # 07 - Apr 21st, 2022 - Adding Spearman and adjusting kwargs
-    #                       Added correlation method to the title/filename
-    # 08 - Jan 26th, 2023 - Adjusting new strategies
-    # 09 -
-
-    # Insights:
-    # Add rotation to x_axis labels (need to check anchor),
-    # 
-
     # Program ----------------------------------------------------------
     data = DataFrame.copy()
 
@@ -52,7 +59,7 @@ def plot_heatmap(DataFrame, title=None, columns="all", decimals=2,
 
     title = f"{title} ({method})"
 
-    # Columns
+    # Columns select    
     if(columns != "all"):
         if(isinstance(columns, str) == True):
             columns = columns.split(",")
@@ -62,6 +69,15 @@ def plot_heatmap(DataFrame, title=None, columns="all", decimals=2,
     else:
         columns = data.columns.tolist()
 
+
+    # Remove string columns
+    cols_remove = []
+    for col in columns:
+        if(is_numeric_dtype(data[col]) == False):
+            cols_remove.append(col)
+
+    data = data.drop(columns=cols_remove)
+    
 
     # Data Processing
     no_rows = len(columns)
@@ -76,18 +92,17 @@ def plot_heatmap(DataFrame, title=None, columns="all", decimals=2,
 
    
     # Plot
-    x_size, y_size = 8, 4.5     # Using widescreen ratio (16:9)
-
-    fig = plt.figure(figsize=[x_size, y_size])
+    fig = plt.figure(figsize=[8, 4.5])
     ax = fig.add_subplot()
-    im = ax.imshow(corr, cmap=colormap, aspect=y_size/x_size)
+    im = ax.imshow(corr, cmap=colormap, aspect=(4.5 / 8))
 
-    fig.suptitle(title, fontsize=fontsize+1, fontweight="bold")
+    fig.suptitle(title, fontsize=10, fontweight="bold", x=0.98, ha="right")
 
     ax.set_yticks(np.arange(start=0, stop=no_rows))
     ax.set_xticks(np.arange(start=0, stop=no_rows))
-    ax.set_yticklabels(columns, rotation=0, fontsize=fontsize)
-    ax.set_xticklabels(columns, rotation=90, fontsize=fontsize)
+    ax.set_yticklabels(columns, rotation=0, fontsize=9)
+    ax.set_xticklabels(columns, rotation=90, fontsize=9)
+
 
     # Creating Text Annotations
     for i in range(0, len(columns)):
@@ -99,7 +114,7 @@ def plot_heatmap(DataFrame, title=None, columns="all", decimals=2,
                 else: textcolor = "black"
 
                 text = ax.text(j, i, value, ha="center", va="center",
-                               color=textcolor, fontsize=fontsize-1)
+                               color=textcolor, fontsize=8)
 
 
     # Printing
