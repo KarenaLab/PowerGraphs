859b76d04ebee511e8f62b46f21cfc58306148eb
Seb Karena <EKChikui@gmail.com>
2024-01-04 19:23:14 -0300

    Refactoring code

--- 01_comparison/line/plot_line.py
+++ 01_comparison/line/plot_line.py
@@ -4,10 +4,10 @@
 # 01 - Jan 21st, 2023 - Starter
 # 02 - Jun 12th, 2023 - Adjust params
 #    - Jan 03rd, 2024 - Set legend over all items
-#
+# 03 - Jan 04th, 2024 - (( refactoring ))
 
 
-# Insights and bugfix
+# Insights, improvements and bugfix
 # 
 
 
@@ -18,101 +18,58 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+# -----------------------------------------------------------------------
 
-def plot_line(DataFrame, title=None, xlabel=None, ylabel=None, index=None,
-              columns="all", legend=True, legend_loc="best", grid_axis="both",
-              marker=False, remove_axis=False, savefig=False, verbose=True):
-
-    """
-    Plots a graph with lines.
-
-    Variables:
-    * DataFrame = Data to be plot,
-    * title = Title for the plot (and the name for the file if savefig=True),
-    * xlabel = label for x axis,
-    * ylabel = label for y axis,
-    * index = None or the name of the column to be the index. If None will
-      be used the index of DataFrame,
-    * columns = "all" or a list of items to be plot,
-    * legend = True* or False,
-    * legend_loc = "best"*, "upper left", "upper right", "lower left" or "lower right",
-    * grid_axis = "both"*, "x" or "y",
-    * marker = True* or False for a marker in data points,
-    * savefig = True or False for saving the plot as a figure using title as name,
-    * verbose = True* or False for messages.
+def plot_line(x, y, title=None, xlabel=None, ylabel=None, color="navy",
+              linewidth=1.5, grid="both", remove_axis=False,
+              savefig=False, verbose=True):
 
     """
-    data = DataFrame.copy()
+    Plots a line graph with **x** and **y**.
+    Objective is to make life easier for a first simple plot and be a
+    support for more detailed graphs.
 
-    # Data Preparation
+    """
+    # Data preparation
+    x = np.array(x)
+    y = np.array(y)
+
     # Title
     if(title == None):
         title = "Line plot"
-        
-    # Index
-    if(index == None):
-        _index = np.array(data.index)
 
-    else:
-        _index = np.array(data[index])
-        data = data.drop(columns=[index])
-        
-    # Columns
-    if(columns != "all"):
-        if(isinstance(columns, str) == True):
-            columns = columns.split(",")
-            
-        data = data[columns]
-
-    else:
-        columns = data.columns.tolist()
-
-
-    # Colors
-    colors = ["navy", "darkred", "darkgreen", "orange",
-              "cornflowerblue", "indianred", "yellowgreen", "bisque"]
-           
-    nlines = data.shape[1]
-
+    # Grid (Default mode is **both**)
+    grid_options = ["both", "y", "x"]
+    if(grid_options.count(grid) == 0):
+        grid = "both"
 
     # RC Params
     plt.rcParams["font.family"] = "Helvetica"
-    plt.rcParams["figure.dpi"] = 180
+    plt.rcParams["figure.dpi"] = 150
     plt.rcParams["ps.papersize"] = "A4"
     plt.rcParams["xtick.direction"] = "inout"
     plt.rcParams["ytick.direction"] = "inout"
-    
-    
+
     # Plot
-    fig = plt.figure(figsize=[6, 3.375])
+    fig = plt.figure(figsize=[6, 3.375])        # Widescreen [16:9]
     fig.suptitle(title, fontsize=10, fontweight="bold", x=0.98, ha="right")
+    ax = plt.axes()
 
-    ax = plt.axes()   
-    _zorder=2   
-    for y, color in list(zip(columns, colors[0:nlines])):
-        plt.plot(_index, data[y], color=color, label=y, zorder=_zorder)
+    plt.plot(x, y, color=color, linewidth=linewidth, zorder=20)
 
-        if(marker == True):
-            plt.scatter(_index, data[y], color=color, edgecolor="white", zorder=_zorder+1)
+    plt.grid(axis=grid, color="lightgrey", linestyle="--", linewidth=0.5, zorder=10)
 
-        _zorder = _zorder + 1
+    if(xlabel != None):
+        plt.xlabel(xlabel, loc="right")
 
-    plt.grid(axis=grid_axis, color="lightgrey", linestyle="--", linewidth=0.5, zorder=1)   
+    if(ylabel != None):
+        plt.ylabel(ylabel, loc="top")
 
     if(remove_axis == True):
         plt.tick_params(length=0,labelleft="on", labelbottom="on")
         ax.spines.right.set_visible(False)
         ax.spines.top.set_visible(False)
         ax.spines.left.set_visible(False)
-        
-    if(ylabel != None):
-        plt.ylabel(ylabel, loc="top")
-
-    if(xlabel != None):
-        plt.xlabel(xlabel, loc="right")
-
-    if(len(columns) > 1 and legend == True):
-        plt.legend(loc=legend_loc, framealpha=1).set_zorder(99)  
 
 
     # Printing
