0ea28170e0d32f3b0b0545f901a59fc3900141cc
Seb Karena <EKChikui@gmail.com>
2024-01-03 23:52:02 -0300

    Refactoring code

--- 04_relationship/scatter_simple/plot_scattersimple.py
+++ 04_relationship/scatter_simple/plot_scattersimple.py
@@ -1,34 +1,74 @@
+# Scatter plot - Simple [P286] ------------------------------------------
 
-def ScatterSimple(Data_x, Data_y, Title_x, Title_y):
-
-    # Versions ----------------------------------------------------------
-
-    # 01 - Starter
-
-
-    # Program -----------------------------------------------------------
-    
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import matplotlib.axes as axes
+# Versions
+# 01 - Fev 14th, 2023 - Starter
+#    - Jan 03rd, 2024 - Rafactoring
+#
 
 
-    x = Data_x
-    y = Data_y
+# Insights, improvements and bugfix
+#
 
-    
-    fig, ax = plt.subplots(figsize= (12, 8.5))   # A4 Ratio (1,4)
-    
-    ax.scatter(x= x, y= y)
 
-    ax.set_xlabel(Title_x)
-    ax.set_ylabel(Title_y)
-    ax.grid(color= "lightgray", linestyle= "dashed", linewidth= 0.5)
+# Libraries
+import numpy as np
+import pandas as pd
 
+import matplotlib.pyplot as plt
+
+
+
+# -----------------------------------------------------------------------
+
+def plot_scattersimple(x, y, title=None, xlabel=None, ylabel=None,
+                       alpha=0.8, mark_size=30, verbose=True, savefig=False):
+    """
+
+    More info:
+    https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.scatter.html
+
+    """
+    # Data preparation
+    x = np.array(x)
+    y = np.array(y)
+
+    # Title
+    if(title == None):
+        title = "Scatter plot"
+
+
+    # RC Params
+    plt.rcParams["font.family"] = "Helvetica"
+    plt.rcParams["figure.dpi"] = 150
+    plt.rcParams["ps.papersize"] = "A4"
+    plt.rcParams["xtick.direction"] = "inout"
+    plt.rcParams["ytick.direction"] = "inout"
+
+    # Plot
+    fig = plt.figure(figsize=[6, 3.375])    # Widescreen 16:9
+    fig.suptitle(title, fontsize=10, fontweight="bold", x=0.98, ha="right")
+
+    plt.scatter(x, y, s=mark_size, color="navy", edgecolor="white", alpha=alpha, zorder=20)
+
+    if(xlabel != None): plt.xlabel(xlabel, loc="center")
+    if(ylabel != None): plt.ylabel(ylabel, loc="center")
+
+    plt.grid(axis="both", color="grey", linestyle="--", linewidth=0.5, zorder=5)       
 
     plt.tight_layout()
-    plt.show()
 
-    return None
+    if(savefig == True):
+        plt.savefig(title, dpi=320)
+
+        if(verbose == True):
+            print(f' > saved plot as "{title}.png"')
+
+    else:
+        plt.show()
+
+    plt.close(fig)
+
+    return None    
 
 
+# end
