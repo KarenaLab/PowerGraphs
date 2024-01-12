6602bf81ca079f84fbd342b3d33be178266cdf54
Seb Karena <EKChikui@gmail.com>
2024-01-11 21:27:16 -0300

    Refactoring

--- 04_relationship/scatter_histogram/plot_scatterhist.py
+++ 04_relationship/scatter_histogram/plot_scatterhist.py
@@ -3,80 +3,103 @@
 # Libraries
 import numpy as np
 import pandas as pd
+
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
 
 # Versions
-# 01 - 01st Jan 2021 - Starter
-# 02 - 30th Jan 2021 - Adding Labels into axis at Scatter Graph
-# 03 - 03rd Feb 2021 - Adijusting fig size for best print (A4 Ratio)
-# 04 -
+# 01 - Jan 01st, 2021 - Starter
+# 02 - Jan 30th, 2021 - Add labels into axis
+# 03 - Feb 03rd, 2021 - Adjust figsize for best print (A4 Ratio)
+# 04 - Jan 11th, 2024 - Refactoring
+#
 
 # Insights, improvements and bugfix
 #
 
 
-def ScatterHist(Title, Label_X, Data_X, Label_Y, Data_Y):
+# -----------------------------------------------------------------------
+
+def plot_scatterhist(x, y, title=None, xlabel=None, ylabel=None, color="navy",
+                       alpha=0.8, mark_size=20, bins="sqrt",
+                       savefig=False, verbose=True):
     """
 
+    More info:
+    https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.scatter.html
 
     """
-    # Creating Figure
-    fig = plt.figure(figsize= (10, 8.5))        # A4 Ratio (1.4)
+    # Data preparation
+    x = np.array(x)
+    y = np.array(y)
 
-    gs = fig.add_gridspec(ncols= 2, nrows= 2,
-                          width_ratios= (3, 1), height_ratios= (1, 3),
-                          left= 0.1, right= 0.9, bottom= 0.1, top= 0.9,
-                          wspace= 0.1, hspace= 0.1)
+    # Title
+    if(title == None):
+        title = "Scatter with Histogram"
 
-
-    # Creating Plotting
-    ax0 = fig.add_subplot(gs[1, 0])  # Main Plot
-    ax1 = fig.add_subplot(gs[0, 0], sharex = ax0)
-    ax2 = fig.add_subplot(gs[1, 1], sharey = ax0)
-
-
-    # Setting/Adjusting Visual Paramenters
-
-    fig.suptitle("Scatter Plot - " + Title, fontsize = 16)
-
-    ax1.tick_params(axis= "x", labelbottom= False)
-    ax2.tick_params(axis= "y", labelleft= False)
-
-    Grid_Color = "grey"
-    Grid_Linestyle = "--"
-    Grid_Linewidth = 0.5
-
-    ax0.grid(color= Grid_Color, linestyle= Grid_Linestyle, linewidth= Grid_Linewidth)
-    ax0.set_axisbelow(True)
-
-    ax1.grid(color= Grid_Color, linestyle= Grid_Linestyle, linewidth= Grid_Linewidth)
-    ax1.set_axisbelow(True)
-
-    ax2.grid(color= Grid_Color, linestyle= Grid_Linestyle, linewidth= Grid_Linewidth)
-    ax2.set_axisbelow(True)
+    # RC Params
+    plt.rcParams["font.family"] = "Helvetica"
+    plt.rcParams["figure.dpi"] = 120
+    plt.rcParams["ps.papersize"] = "A4"
+    plt.rcParams["xtick.direction"] = "inout"
+    plt.rcParams["ytick.direction"] = "inout"
 
 
-    # Graph 0 = Scatter
-    ax0.scatter(Data_X, Data_Y, alpha= 0.2, edgecolor= "white")
-    ax0.set_xlabel(Label_X)
-    ax0.set_ylabel(Label_Y)
+    # Plot
+    # Figure
+    fig = plt.figure(figsize=[6, 3.375])    # Widescreen 16:9
+    grd = fig.add_gridspec(ncols=2, width_ratios=[3, 1],
+                           nrows=2, height_ratios=[1, 3])
+
+    ax0 = fig.add_subplot(grd[1, 0])                # Scatter (main plot)
+    ax1 = fig.add_subplot(grd[0, 0], sharex=ax0)    # Histogram for x
+    ax2 = fig.add_subplot(grd[1, 1], sharey=ax0)    # Histogram for y
+
+    fig.suptitle(title, fontsize=10, fontweight="bold", x=0.98, ha="right")
+
+    # Scatter plot
+    ax0.scatter(x, y, s=mark_size, color=color, edgecolor="white", alpha=alpha, zorder=20)
+    ax0.grid(axis="both", color="grey", linestyle="--", linewidth=0.5, zorder=5)       
+
+
+    if(xlabel != None):
+        ax0.set_xlabel(xlabel, loc="center")
+
+    if(ylabel != None):
+        ax0.set_ylabel(ylabel, loc="center")
+
+
+    # Histogram for x
+    ax1.hist(x, bins=bins, color=color, orientation="vertical", zorder=20)
+    ax1.grid(axis="both", color="grey", linestyle="--", linewidth=0.5, zorder=5)       
+    ax1.tick_params(axis="x", labelbottom=False)
+
+
+    # Histogram for y
+    ax2.hist(y, bins=bins, color=color, orientation="horizontal", zorder=20)
+    ax2.grid(axis="both", color="grey", linestyle="--", linewidth=0.5, zorder=5)       
+    ax2.tick_params(axis="y", labelleft=False)
     
 
-    # Graph 1 = Histogram of X
-    Data_Size = Data_X.size
-    bins_x = int((Data_Size ** (1/2)) + 0.5)
-    
-    ax1.hist(x= Data_X, bins= bins_x, orientation= "vertical")
-      
+    # Printing
+    plt.tight_layout()
 
-    # Graph 2 = Histogram of Y
-    Data_Size = Data_Y.size
-    bins_y = int((Data_Size ** (1/2)) + 0.5)
+    if(savefig == True):
+        plt.savefig(title, dpi=320)
 
-    ax2.hist(x= Data_Y, bins= bins_y, orientation= "horizontal")
-    
+        if(verbose == True):
+            print(f' > saved plot as "{title}.png"')
 
-    # Plotting (Out)
-    plt.savefig(Title, dpi= 240)
-    plt.show()
+    else:
+        plt.show()
+
+    plt.close(fig)
+
+    return None    
+
+
+# end
+
+
 
