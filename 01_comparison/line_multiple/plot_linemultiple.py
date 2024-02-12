9343375f2c39dc451967546c847aa8cc3d287d07
Seb Karena <EKChikui@gmail.com>
2024-02-11 23:03:35 -0300

    Update plot_linemultiple.py

--- 01_comparison/line_multiple/plot_linemultiple.py
+++ 01_comparison/line_multiple/plot_linemultiple.py
@@ -57,7 +57,8 @@ def plot_linemultiple(DataFrame, columns=None, title=None, xlabel=None,
     plt.rcParams["ps.papersize"] = "A4"
     plt.rcParams["xtick.direction"] = "inout"
     plt.rcParams["ytick.direction"] = "inout"
-
+    plt.rcParams["xtick.major.size"] = 3.5
+    plt.rcParams["ytick.major.size"] = 3.5
 
     # Plot
     fig = plt.figure(figsize=[6, 3.375])        # Widescreen [16:9]
