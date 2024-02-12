4090e11ed5047af14918a6d70549c5eb5777003c
Seb Karena <EKChikui@gmail.com>
2024-02-12 18:38:16 -0300

    improve: standard tick size (rcparams)

--- 02_distribution/histogram_box/plot_histbox.py
+++ 02_distribution/histogram_box/plot_histbox.py
@@ -121,7 +121,8 @@ def plot_histbox(data, title=None, xlabel=None, bins="sqrt",
     plt.rcParams["ps.papersize"] = "A4"
     plt.rcParams["xtick.direction"] = "inout"
     plt.rcParams["ytick.direction"] = "inout"
-
+    plt.rcParams["xtick.major.size"] = 3.5
+    plt.rcParams["ytick.major.size"] = 3.5
 
     # Plot
     fig = plt.figure(figsize=[6, 3.375])        # Widescreen [16:9]
