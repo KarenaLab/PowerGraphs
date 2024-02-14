9c2848a472f10e07f2e318a24d35ad2869d30411
Seb Karena <EKChikui@gmail.com>
2024-02-14 19:06:18 -0300

    improve: standard axis ticks size

--- 04_relationship/scatter_simple/plot_scattersimple.py
+++ 04_relationship/scatter_simple/plot_scattersimple.py
@@ -41,6 +41,8 @@ def plot_scattersimple(x, y, title=None, xlabel=None, ylabel=None, color="navy",
     plt.rcParams["ps.papersize"] = "A4"
     plt.rcParams["xtick.direction"] = "inout"
     plt.rcParams["ytick.direction"] = "inout"
+    plt.rcParams["xtick.major.size"] = 3.5
+    plt.rcParams["ytick.major.size"] = 3.5
 
     # Plot
     fig = plt.figure(figsize=[6, 3.375])    # Widescreen 16:9
