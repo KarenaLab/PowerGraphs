6d03881e265be931b9fe96a59bcfdcf7d33382a2
Seb Karena <EKChikui@gmail.com>
2024-02-13 21:07:28 -0300

    improve: standard ticks size

--- 02_distribution/histogram_simple/plot_histogram.py
+++ 02_distribution/histogram_simple/plot_histogram.py
@@ -119,7 +119,8 @@ def plot_histogram(data, title=None, xlabel=None, bins="sqrt",
     plt.rcParams["ps.papersize"] = "A4"
     plt.rcParams["xtick.direction"] = "inout"
     plt.rcParams["ytick.direction"] = "inout"
-
+    plt.rcParams["xtick.major.size"] = 3.5
+    plt.rcParams["ytick.major.size"] = 3.5
 
     # Plot
     fig = plt.figure(figsize=[6, 3.375])        # Widescreen [16:9]
