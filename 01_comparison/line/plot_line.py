21402311f20c6e86b28a0e87d0b235586299138c
Seb Karena <EKChikui@gmail.com>
2024-02-26 19:39:32 -0300

    improve: hline and vline feature

--- 01_comparison/line/plot_line.py
+++ 01_comparison/line/plot_line.py
@@ -5,7 +5,8 @@
 # 02 - Jun 12th, 2023 - Adjust params
 #    - Jan 03rd, 2024 - Set legend over all items
 # 03 - Jan 04th, 2024 - (( refactoring ))
-#      Fev 06th, 2024 - Add grid default selection
+#      Feb 06th, 2024 - Add grid default selection
+#      Feb 26th, 2024 - Add hline and vline option
 #
 
 
@@ -23,7 +24,8 @@ import matplotlib.pyplot as plt
 # -----------------------------------------------------------------------
 
 def plot_line(x, y, title=None, xlabel=None, ylabel=None, color="navy",
-              linewidth=1.5, grid="both", remove_axis=False,
+              linewidth=1.5, grid="both", vline=None, hline=None,
+              remove_axis=False,
               savefig=False, verbose=True):
 
     """
@@ -43,9 +45,9 @@ def plot_line(x, y, title=None, xlabel=None, ylabel=None, color="navy",
     # Grid Axis
     grid_default = "both"
     grid_list = ["x", "y", "both"]
-    if(grid_list.count(grid_axes) == 0):
+    if(grid_list.count(grid) == 0):
         print(f' >>> Error: "grid_axis" option not valid. Using "{grid_default}" as forced option.')
-        grid_axes = grid_default[:]
+        grid = grid_default[:]
 
 
     # RC Params
@@ -67,6 +69,14 @@ def plot_line(x, y, title=None, xlabel=None, ylabel=None, color="navy",
 
     plt.grid(axis=grid, color="lightgrey", linestyle="--", linewidth=0.5, zorder=10)
 
+    if(isinstance(hline, list) == True):
+        for y in hline:
+            plt.axhline(y=y, color="darkred", linewidth=0.8, zorder=19)
+
+    if(isinstance(vline, list) == True):
+        for x in vline:
+            plt.axvline(x=x, color="darkred", linewidth=0.8, zorder=19)
+            
     if(xlabel != None):
         plt.xlabel(xlabel, loc="right")
 
