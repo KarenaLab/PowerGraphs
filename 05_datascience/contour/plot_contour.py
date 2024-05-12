ad0ec2b0f48dfec6fc915ea2c0a273d00c326b24
Seb Karena <EKChikui@gmail.com>
2024-05-12 18:50:59 -0300

    Update plot_contour.py

--- 05_datascience/contour/plot_contour.py
+++ 05_datascience/contour/plot_contour.py
@@ -1,8 +1,7 @@
-# Name [Pxxx]
-# (optional) Short description
+# Countour 2D [P426]
 
 # Versions
-# 01 - Date - Starter
+# 01 - May 12th, 2024 - Starter
 # 02 -
 
 
@@ -11,23 +10,20 @@
 
 
 # Libraries
-from collections import namedtuple
-
 import numpy as np
 import pandas as pd
-import scipy.stats as st
 
 import matplotlib.pyplot as plt
 
 
-
 # ----------------------------------------------------------------------
-def name():
+def plot_countour():
     """
     Description of the function
     Information about variables and **kwargs
 
     """
+    pass
 
     return None    
 
