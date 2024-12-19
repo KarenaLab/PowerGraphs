9e9c3f6727a73da762b44b5dc3048a96abc6b509
Seb Karena <EKChikui@gmail.com>
2024-12-18 22:57:48 -0300

    Create plot_findzeros.py

---
+++ 05_datascience/find_zeros/plot_findzeros.py
@@ -0,0 +1,32 @@
+# Name [P448] - PowerGraphs - Find Zeros
+
+# Versions
+# 01 - Date - Starter
+# 02 -
+
+
+# Insights, improvements and bugfix
+#
+
+
+# Libraries
+from collections import namedtuple
+
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+
+import matplotlib.pyplot as plt
+
+
+
+# ----------------------------------------------------------------------
+def name():
+    """
+    Description of the function
+    Information about variables and **kwargs
+
+    """
+
+    return None    
+
