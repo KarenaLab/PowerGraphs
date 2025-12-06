a42efa79c6387d8756652d268a4e287ab4b4f79a
Seb Karena <EKChikui@gmail.com>
2025-12-05 23:47:33 -0300

    Update plot_scattersimple.py

--- 04_relationship/scatter_simple/plot_scattersimple.py
+++ 04_relationship/scatter_simple/plot_scattersimple.py
@@ -1,10 +1,5 @@
 # Scatter plot - Simple [P286] ------------------------------------------
 
-# Versions
-# 01 - Fev 14th, 2023 - Starter
-#    - Jan 03rd, 2024 - Rafactoring code (new code style)
-#
-
 
 # Insights, improvements and bugfix
 #
@@ -17,9 +12,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-
 # -----------------------------------------------------------------------
-
 def plot_scattersimple(x, y, title=None, xlabel=None, ylabel=None, color="navy",
                        alpha=0.8, mark_size=20, savefig=False, verbose=True):
     """
@@ -36,13 +29,8 @@ def plot_scattersimple(x, y, title=None, xlabel=None, ylabel=None, color="navy",
     if(title == None): title = "Scatter plot"
 
     # RC Params
-    plt.rcParams["font.family"] = "Helvetica"
-    plt.rcParams["figure.dpi"] = 120
-    plt.rcParams["ps.papersize"] = "A4"
-    plt.rcParams["xtick.direction"] = "inout"
-    plt.rcParams["ytick.direction"] = "inout"
-    plt.rcParams["xtick.major.size"] = 3.5
-    plt.rcParams["ytick.major.size"] = 3.5
+    set_rcparams()
+    
 
     # Plot
     fig = plt.figure(figsize=[6, 3.375])    # Widescreen 16:9
@@ -74,4 +62,17 @@ def plot_scattersimple(x, y, title=None, xlabel=None, ylabel=None, color="navy",
 
     return None    
 
-# end
+
+def set_rcparams():
+    plt.rcParams["font.family"] = "Helvetica"
+    plt.rcParams["font.size"] = 8
+    plt.rcParams["figure.dpi"] = 120
+    plt.rcParams["ps.papersize"] = "A4"
+    plt.rcParams["xtick.direction"] = "inout"
+    plt.rcParams["ytick.direction"] = "inout"
+    plt.rcParams["xtick.major.size"] = 3.5
+    plt.rcParams["ytick.major.size"] = 0
+
+    return None
+
+
